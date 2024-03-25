da51c3530af28d21328b831c5257daf24d05fc3f
thinh-tran <thinh@inter-soft.com>
2024-03-25 11:00:22 +0700

    uppdate pull.sh

--- pull.sh
+++ pull.sh
@@ -1,1 +1,1 @@
-curl -OJL ./ps-modules https://github.com/PrestaShop/PrestaShop/releases/download/1.7.8.0/prestashop_1.7.8.0.zip
+curl -o ./ps-modules/file.zip https://github.com/PrestaShop/PrestaShop/releases/download/1.7.8.0/prestashop_1.7.8.0.zip
